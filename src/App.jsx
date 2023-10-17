794eadd31fd1c340a089504fe2a7bea85ab70058
Aziz Zulfikorov <tokzor123@yahoo.com>
2023-10-17 13:09:39 +0500

    still working on my new React app leaning React concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,282 +1,91 @@
 import './App.css'
 import {useState} from 'react'
 
-/*const History = (props) => {
-  if(props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
+const Button = (props) =>{
+
+  const {handleClick, text} = props
+
+  return(
+    <div style={{display:'inline-block'}}>
+      
+      <button onClick={handleClick}>{text}</button>
+      
     </div>
+    
+
   )
 }
 
-const Button = ({ handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-  
-const Display = props => <div>{props.value}</div>
+const Statistics = ({good, neutral, bad}) => {
 
+  //const {good, neutral, bad} = props
 
-const Button = (props) => (
-  <button onClick = {props.handleClick}>
-    {props.text}
-  </button>
-)*/
+  const all = good+neutral+bad
+  let average;
+  let positive;
 
-function App() {
+  if(all===0){
+    average = 0;
+    positive = 0;
+  }
+   else {
+    average = (good-bad)/all
+    positive = (good/all)*100
+   }
 
-  /*
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)*/
+   return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${positive}%`} />
+            
+    </div>
 
-  /*setTimeout(
-    () => setCounter (counter + 1),
-    1000
+   )
+
+}
+
+const StatisticLine = (props) => {
+
+  return(
+   
+      <table>
+        <tbody>
+        <tr>
+          <td style={{width:70,textAlign:'left'}}>{props.text}:</td>
+          <td>{props.value}</td>
+        </tr>
+        </tbody>
+      </table>
+              
   )
+ 
+}
 
-  const handleClick = () => {
-    console.log('clicked')
-    setCounter (counter + 1)
-  }
- */
-/*
-  const increaseByOne = () => {
-    console.log(('increasing, value before', counter))
-    setCounter(counter + 1)
-  }
+function App()  {
 
-  const decreaseByOne = () =>{
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  } 
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  const Display = ({counter}) => <div>{counter}</div>
-      
-  const Button = ({handleClick,text}) => <button onClick = {handleClick}>{text}</button>
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  
+   
+   return (
+    <div>
+      <h2>give feedback</h2>
      
-  return (
-    <div>
-      <Display counter={counter} />
-
-      <Button 
-       handleClick = {increaseByOne}
-       text='plus'
-      />
-        
+       <Button handleClick={()=>setGood(good + 1)} text={'good'}/>
+       <Button handleClick={()=>setNeutral(neutral + 1)} text={'neutral'}/>
+       <Button handleClick={()=> setBad(bad + 1)} text={'bad'}/>
+       
+      <h2>statistics</h2>
+      {good||neutral||bad ? <Statistics good={good} neutral={neutral} bad={bad}/>
+       :'No feedback given'
+      }      
       
-      <Button 
-       handleClick = {setToZero}
-       text='zero'
-      />
-
-      <Button 
-       handleClick = {decreaseByOne}
-       text='minus'
-       />
-
-    </div>
-    
-  )
-
-  const [clicks, setClicks] = useState({left: 0, right: 0})
-  
- const handleLeftClick = () => {
-  const newClicks = {
-    ...clicks,
-    left: clicks.left + 1
-  }
-  setClicks(newClicks)
-  console.log(newClicks)
- }
-
- const handleRightClick = () => {
-  const newClicks = {
-    ...clicks,
-    right: clicks.right + 1
-  }
-  setClicks(newClicks)
-  console.log(newClicks)
- }
-
- console.log(clicks)
- 
-
-  return(
-    <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      {clicks.right}
-    </div>
-  )*/
-  
-/*
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-   // console.log('left before',left)
-   const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    //console.log('left after', left)
-    setTotal(updatedLeft + right)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
-  }
-
-  return (
-    <div>
-      {left}
-      <Button handleClick = {handleLeftClick} text='left'/>
-      <Button handleClick = {handleRightClick} text='right'/>
-      {right}
-      <History allClicks = {allClicks} />
-      <p>{allClicks.join(' ')}</p>
-      <p>total {total}</p>
-    </div>
-  )*/
-
-  const [value, setValue] = useState(10)
-
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
-    
-  
-  
-
-  return(
-    <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
-
-    </div>
-  )
-
-
-
-
-
-
-  const course = {
-
-    name:'Half Stack application development',
-    parts:[
-    {
-      name: 'Fundamental of React',
-      exercises: 10
-    },
-    
-     {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    
-     {
-      name: 'State of a component',
-      exercises: 14
-    }
-
-  ]
-}
-  
-const Hello = ({name, age}) => {
- //const{name, age} = props
- const bornYear = () => new Date().getFullYear() - age
- 
-
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old.
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-
-const App2 = () =>{
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10}/>
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
-
-
-  const Header = (props) =>{
-   // console.log(props);
-    return(
-      <h1>{props.course.name}</h1>
-    )
-
-  }
-
-  const Content = (props) =>{
-   // console.log(props)
-     return(
-      <div>
-        <p>{props.course.parts[0].name} - {props.course.parts[0].exercises}</p>
-        <p>{props.course.parts[1].name} - {props.course.parts[1].exercises}</p>
-        <p>{props.course.parts[2].name} - {props.course.parts[2].exercises}</p>
-
-      </div>
-     )
-  }
-
- 
-
-  const Total = (props) =>{
-     return (
-      <div>
-        <p>
-          Number of exercises : {props.course.parts[0].exercises
-           + props.course.parts[1].exercises+props.course.parts[2].exercises}.
-        </p>
-      </div>
-     )
-  }
-
-  return (
-    <div>
-     <Header course = {course}/>
-     <Content course = {course}/>
-     <Total course = {course}/>
-     <App2 />
     </div>
   )
 }
