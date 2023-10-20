b69c5357a16e27ad126c22bc4c3b9e9230b087f8
Aziz Zulfikorov <tokzor123@yahoo.com>
2023-10-20 11:59:31 +0500

    exercises 0 and 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {useState} from 'react'
-
+/*
 const Button = (props) =>{
 
   const {handleClick, text} = props
@@ -86,6 +86,63 @@ function App()  {
        :'No feedback given'
       }      
       
+    </div>
+  )
+}*/
+
+const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    `The first 90 percent of the code accounts for the first 10 percent of the development time...
+    The remaining 10 percent of the code accounts for the other 90 percent of the development time.`,
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState([])
+
+  
+
+
+  const randomAnecdote = () =>{
+
+    const nextAnecdote = Math.floor(Math.random()*anecdotes.length)
+    setSelected(nextAnecdote)
+    
+  }
+
+  const voteForAnecdote = () =>{
+  
+    let selectedAnecdote= [...points,Array.apply(null, new Array(8)).map(Number.prototype.valueOf,0)]
+   
+    setPoints(
+      selectedAnecdote
+    )
+    
+    selectedAnecdote[0][selected] +=1   
+  }
+
+  console.log(selected)
+  console.log(points[0])
+  
+  let mostVotes = points[0].indexOf(Math.max(...points[0]))
+  
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      {anecdotes[selected]}<br/>
+      has {points.length===0?0:points[0][selected]} votes<br/>
+      <button onClick={voteForAnecdote}>vote</button>
+      <button onClick={()=>randomAnecdote()}>next anecdote</button>
+      <h2>Anecdote with most votes</h2>
+      {anecdotes[mostVotes]}<br/>
+      has {Math.max(...points[0])} votes
     </div>
   )
 }
