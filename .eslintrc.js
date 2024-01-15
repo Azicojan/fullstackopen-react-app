744696f0341ec31b545cf2f13477a21e1000c95e
Aziz Zulfikorov <tokzor123@yahoo.com>
2024-01-15 14:23:25 +0500

    finished part#3 and done all of its exercises

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-mixed-spaces-and-tabs */
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 	'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+	 // eslint-disable-next-line no-mixed-spaces-and-tabs
+	 'eqeqeq':'error',
+	 'no-trailing-spaces': 'error',
+	 'object-curly-spacing': [
+      'error', 'always'
+	 ],
+	 'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+	 ],
+	 'no-console': 0
+  },
+}
